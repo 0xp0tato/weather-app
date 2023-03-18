ba5eed72f8d432167a568e8bfe91db4160963477
0xp0tato <ninadmaster05@gmail.com>
2023-03-18 19:45:01 +0530

    Project Initialization

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <span className="App">
+      <div className="text-card">
+        <h1>Weather App</h1>
+        <hr />
+        <input placeholder="Enter City Name"></input>
+      </div>
+      <div className="temp-card">
+        <h1>Weather</h1>
+      </div>
+    </span>
+  );
+}
+
+export default App;
