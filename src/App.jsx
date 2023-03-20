44193c61099b0b1e09083c8fa3bdf43d31ed92c5
0xp0tato <ninadmaster05@gmail.com>
2023-03-20 20:06:53 +0530

    Added icon div and added some css

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ function App() {
   return (
     <span className="App">
       <div className="text-card">
-        <h1>Weather App</h1>
-        <hr />
-        <input placeholder="Enter City Name"></input>
+        <h1 className="app-name">Weather App</h1>
+        <input className="city-input" placeholder="Enter City Name"></input>
       </div>
       <div className="temp-card">
-        <h1>Weather</h1>
+        <h1 className="location">New Delhi</h1>
+        <div className="weather-icon"></div>
+        <h1 className="temperature">13°C</h1>
+        <h1 className="weather-description">Cloudy</h1>
       </div>
     </span>
   );
